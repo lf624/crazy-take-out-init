d4ece6d28bdb2f14f9873b117ff4146ac7e8c4f2
fei <2973318327@qq.com>
2024-12-30 11:11:00 +0800

    optimize custom JacksonObjectMapper

--- crazy-common/src/main/java/com/crazy/json/JacksonObjectMapper.java
+++ crazy-common/src/main/java/com/crazy/json/JacksonObjectMapper.java
@@ -2,24 +2,43 @@ package com.crazy.json;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class JacksonObjectMapper extends ObjectMapper {
 
     private static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
 
     public JacksonObjectMapper() {
         super();
 
-        // 设置全局的日期格式化
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addSerializer(java.time.LocalDateTime.class,
-                new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)));
-        // 注册 JavaTimeModule 到 ObjectMapper
-        this.registerModule(javaTimeModule);
+        // 创建格式化器
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT);
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT);
+        // 创建并注册 SimpleModule
+        SimpleModule simpleModule = new SimpleModule()
+                .addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter))
+                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormatter))
+                .addSerializer(LocalDate.class, new LocalDateSerializer(dateFormatter))
+                .addDeserializer(LocalDate.class, new LocalDateDeserializer(dateFormatter))
+                .addSerializer(LocalTime.class, new LocalTimeSerializer(timeFormatter))
+                .addDeserializer(LocalTime.class, new LocalTimeDeserializer(timeFormatter));
+
+        // 注册模块到 ObjectMapper
+        this.registerModule(simpleModule);
 
         // 忽略反序列化时多余的字段
         this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
