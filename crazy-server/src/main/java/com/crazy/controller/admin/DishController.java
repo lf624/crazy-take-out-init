394360b3d68a755e7f30e3fd476dc2aa0519e618
fei <2973318327@qq.com>
2025-01-07 10:38:41 +0800

    add dish cache

--- crazy-server/src/main/java/com/crazy/controller/admin/DishController.java
+++ crazy-server/src/main/java/com/crazy/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @RestController
@@ -24,11 +26,18 @@ public class DishController {
     @Autowired
     DishService dishService;
 
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
     @PostMapping
     @Operation(summary = "新增菜品")
     public Result<String> save(@RequestBody DishDTO dishDTO) {
         log.info("newly add dish: {}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        // 清理菜品缓存
+        String key = "dish_" + dishDTO.getCategoryId();
+        cleanCache(key);
         return Result.success();
     }
 
@@ -45,6 +54,9 @@ public class DishController {
     public Result<String> delete(@RequestParam List<Long> ids) {
         log.info("batch delete: {}", ids);
         dishService.deleteBatch(ids);
+
+        // 将所有菜品缓存清理掉
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -61,6 +73,9 @@ public class DishController {
     public Result<String> updateDishWithFlavor(@RequestBody DishDTO dishDTO) {
         log.info("update dish: {}", dishDTO);
         dishService.updateDishWithFlavor(dishDTO);
+
+        // 清理菜品缓存
+        cleanCache("dish_" + dishDTO.getCategoryId());
         return Result.success();
     }
 
@@ -76,6 +91,14 @@ public class DishController {
     public Result<String> startOrStop(@PathVariable Integer status, @RequestParam Long id) {
         log.info("change dish {} to status: {}", id, status);
         dishService.changeStatus(status, id);
+
+        // 清理所有菜品缓存
+        cleanCache("dish_*");
         return Result.success();
+    }
+
+    private void cleanCache(String pattern) {
+        Set<String> keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
     }
 }
--- crazy-server/src/main/java/com/crazy/controller/user/DishController.java
+++ crazy-server/src/main/java/com/crazy/controller/user/DishController.java
@@ -6,6 +6,7 @@ import com.crazy.vo.DishVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,10 +22,24 @@ public class DishController {
     @Autowired
     DishService dishService;
 
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
     @GetMapping("/list")
     @Operation(summary = "根据分类id查询菜品")
+    @SuppressWarnings("unchecked")
     public Result<List<DishVO>> list(@RequestParam Long categoryId) {
-        List<DishVO> dishVOList = dishService.listWithFlavor(categoryId);
-        return Result.success(dishVOList);
+        // 先查看缓存
+        String key = "dish_" + categoryId;
+        List<DishVO> list = (List<DishVO>) redisTemplate.opsForValue().get(key);
+
+        // 如果缓存中有，直接返回
+        if(list != null && !list.isEmpty())
+            return Result.success(list);
+        // 若不存在，查询数据库，并将结果放入缓存中
+        list = dishService.listWithFlavor(categoryId);
+        redisTemplate.opsForValue().set(key, list);
+
+        return Result.success(list);
     }
 }
