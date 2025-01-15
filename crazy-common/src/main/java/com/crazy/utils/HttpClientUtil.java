f019fa77b601bda8dca90792b4e8dafe5b0dfc67
fei <2973318327@qq.com>
2025-01-15 17:37:00 +0800

    modify HttpClientUtil.doGet()

--- crazy-common/src/main/java/com/crazy/utils/HttpClientUtil.java
+++ crazy-common/src/main/java/com/crazy/utils/HttpClientUtil.java
@@ -16,21 +16,25 @@ import java.util.Map;
 @Slf4j
 public class HttpClientUtil {
 
-    // TODO 修改完善
     public static String doGet(String url, Map<String, String> query) {
+        String result = "";
+
         try(CloseableHttpClient httpClient = HttpClients.createDefault()) {
             URIBuilder uriBuilder = new URIBuilder(url);
-            query.forEach(uriBuilder::addParameter);
+            if(query != null && !query.isEmpty())
+                query.forEach(uriBuilder::addParameter);
+            // 创建 GET 请求
             HttpGet httpGet = new HttpGet(uriBuilder.build());
-
+            // 发送请求，接收响应
             CloseableHttpResponse response = httpClient.execute(httpGet);
-            HttpEntity entity = response.getEntity();
-            String json = EntityUtils.toString(entity);
-            log.debug("doGet response: {}", json);
-            return json;
-        }catch (IOException | URISyntaxException e) {
+            if(response.getStatusLine().getStatusCode() == 200) {
+                HttpEntity entity = response.getEntity();
+                result = EntityUtils.toString(entity);
+            }
+            log.debug("doGet response: {}", result);
+        } catch (IOException | URISyntaxException e) {
             log.error("http doGet Exception: {}", e.toString());
         }
-        return null;
+        return result;
     }
 }
